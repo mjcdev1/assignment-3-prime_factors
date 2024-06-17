15df58a4ceed9668569362f3e1c6ae4ac7190466
mjcdev1 <mjc.developer1@gmail.com>
2024-06-17 14:39:19 +0000

    Step 1

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,17 @@
+import sys
+import os
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(parent_dir)
+
+import prime
+
+import pytest
+
+def test_not_int():
+    with pytest.raises(ValueError):
+        prime.generate_prime_factors("hello")
+        prime.generate_prime_factors(3.5)
+        prime.generate_prime_factors([2, 3])
--- prime.py
+++ prime.py
@@ -1,3 +1,7 @@
 """
 prime.py -- Write the application code here
 """
+
+def generate_prime_factors(n):
+    if not isinstance(n, int):
+        raise ValueError()
